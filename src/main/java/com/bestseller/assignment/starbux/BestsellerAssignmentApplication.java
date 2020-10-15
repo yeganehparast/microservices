42ffbf78836272f1b771132f2dbc97a4e97dd832
Mehdi Yeganehparast <mehdi.yeganehparast@gmail.com>
2020-10-15 23:48:32 +0200

    Task1-Create-initial-project

---
+++ src/main/java/com/bestseller/assignment/starbux/BestsellerAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.bestseller.assignment.starbux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestsellerAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BestsellerAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bestseller/assignment/starbux/BestsellerAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bestseller.assignment.starbux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestsellerAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
